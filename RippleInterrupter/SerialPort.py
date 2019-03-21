f8c0a7bc9cbde06ba6b6012430dfb64d8d5e682b
wcroughan <croughanb@gmail.com>
2019-03-21 17:45:48 +0000

    added flag when dont actually want to send out stim

--- RippleInterrupter/SerialPort.py
+++ RippleInterrupter/SerialPort.py
@@ -9,6 +9,7 @@ import logging
 BAUDRATE     = 9600
 DEFAULT_PORT = '/dev/ttyS0'
 MODULE_IDENTIFIER = "[SerialPort] "
+REALLY_SEND_STIM = False
 
 class BiphasicPort(serial.Serial):
     """
@@ -16,18 +17,20 @@ class BiphasicPort(serial.Serial):
     """
 
     def __init__(self, port=DEFAULT_PORT, baud=BAUDRATE):
-        serial.Serial.__init__(self, port, baud, timeout=0, stopbits=serial.STOPBITS_ONE, \
-                bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE)
-        logging.info(MODULE_IDENTIFIER + "Serial port initialized.")
+        if REALLY_SEND_STIM:
+            serial.Serial.__init__(self, port, baud, timeout=0, stopbits=serial.STOPBITS_ONE, \
+                    bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE)
+            logging.info(MODULE_IDENTIFIER + "Serial port initialized.")
 
     def sendBiphasicPulse(self):
-        self.setDTR(True)
-        time.sleep(0.0002)
-        self.setDTR(False)
-        time.sleep(0.0001)
-        self.setRTS(True)
-        time.sleep(0.0002)
-        self.setRTS(False)
-        time.sleep(0.001)
-        logging.info(MODULE_IDENTIFIER + "Biphasic pulse delivered.")
+        if REALLY_SEND_STIM:
+            self.setDTR(True)
+            time.sleep(0.0002)
+            self.setDTR(False)
+            time.sleep(0.0001)
+            self.setRTS(True)
+            time.sleep(0.0002)
+            self.setRTS(False)
+            time.sleep(0.001)
+            logging.info(MODULE_IDENTIFIER + "Biphasic pulse delivered.")
         return
